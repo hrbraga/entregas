42b9b5ac4e21b6ff0f76e17e248f1a6ad26392db
hrbraga <hugbraga@gmail.com>
2025-11-18 11:58:52 -0300

    estilização página

--- etiquetas/etiquetas.php
+++ etiquetas/etiquetas.php
@@ -1,42 +1,63 @@
+<?php 
+    require '../config.php'; 
+    require '../auth/custos_auth_check.php'
+?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerador de Etiquetas</title>
+    
+    <link rel="stylesheet" href="../static/css/global.css">
+    <link rel="stylesheet" href="../static/css/selecao.css">
+    <link rel="shortcut icon" href="../static/img/chocolatinho.png" type="image/x-icon">
     <link rel="stylesheet" href="../static/css/etiquetas.css">
+
 </head>
 <body>
 
-    <div class="interface-usuario">
-        <h2>Gerador de Etiquetas</h2>
+    <header>
+        <h1>Gerador de Etiquetas</h1>
+    </header>
 
-        <div class="controles">
-            <label for="codigo_produto">Código (Barra ou Interno):</label>
-            <input type="text" id="codigo_produto" autofocus>
+    <main>
+        <div class="interface-usuario">
+            <div class="controles">
+                <label for="codigo_produto">Código (Barra ou Interno):</label>
+                <input type="text" id="codigo_produto" autofocus>
 
-            <label for="quantidade">Quantidade:</label>
-            <input type="number" id="quantidade" value="1" min="1" max="100">
-            
-            <button id="adicionar_produto">Adicionar</button>
-        </div>
+                <label for="quantidade">Quantidade:</label>
+                <input type="number" id="quantidade" value="1" min="1" max="100">
+                
+                <button id="adicionar_produto">Adicionar</button>
+            </div>
 
-        <h3>Lista para Impressão</h3>
-        <table id="lista_produtos">
-            <thead>
-                <tr>
-                    <th>Produto</th>
-                    <th>Preço NCL</th>
-                    <th>Preço CL</th>
-                    <th>Qtd.</th>
-                    <th>Ação</th>
-                </tr>
-            </thead>
-            <tbody>
+            <h3>Lista para Impressão</h3>
+            <table class="lista_produtos" id="lista_produtos">
+                <thead>
+                    <tr>
+                        <th>Produto</th>
+                        <th>Preço 1 (R$)</th>
+                        <th>Preço 2 (R$)</th>
+                        <th>Qtd.</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
                 </tbody>
-        </table>
+            </table>
 
-        <button id="gerar_etiquetas" class="botao-gerar">Imprimir Etiquetas</button>
-    </div>
+            <button id="gerar_etiquetas" class="botao-gerar">Imprimir Etiquetas</button>
+        </div>
+    </main>
+
+    <footer>
+        <a href="../selecao_ferramentas.php">
+            <p>Voltar a Caixa de Ferramentas</p>
+        </a>
+    </footer>
 
     <div id="container_impressao"></div>
 
